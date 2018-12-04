39d1e755cf6d6d45bda620db03820f1d538517d5
Nau077 <41802678+Nau077@users.noreply.github.com>
2018-12-04 21:22:43 +0300

    Updates

---
+++ precache-manifest.35469176774643bdfacf2e6f465eb11a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "4b099fe073d8051fca7c",
+    "url": "/React-crypto-ticker/static/css/main.2cdb8620.chunk.css"
+  },
+  {
+    "revision": "4b099fe073d8051fca7c",
+    "url": "/React-crypto-ticker/static/js/main.4b099fe0.chunk.js"
+  },
+  {
+    "revision": "6c10c71875cd471ecd52",
+    "url": "/React-crypto-ticker/static/js/1.6c10c718.chunk.js"
+  },
+  {
+    "revision": "bad4751f41bc9dee5c2e",
+    "url": "/React-crypto-ticker/static/js/runtime~main.bad4751f.js"
+  },
+  {
+    "revision": "e0fbb68acb01fd5f7906faaee065533c",
+    "url": "/React-crypto-ticker/static/media/nXzIRGpe5eg.e0fbb68a.jpg"
+  },
+  {
+    "revision": "0a62d8740d3c58b6cb348cfcec6257cb",
+    "url": "/React-crypto-ticker/index.html"
+  }
+];
